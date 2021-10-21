ce62e5d5bfac536ea26dadd9007cf4a3c864e55d
kunpeisatou <kunpei0411@gmail.com>
2021-10-21 01:35:42 +0000

    [Start]4-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '3bfa5e060ac6b7694f4878ae8674b86be68322f61ae98ee3c7d63853ecf23823dbececb48023d0f2e322542aafb1be8bc13259087bd9a98d24de2f6debcb66ff'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #get 'users/show'を削除。下のusersとの重複回避のため。
   devise_for :users
   root to: 'homes#top'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+
+  resources :users, only: [:show, :edit, :update]
 end
