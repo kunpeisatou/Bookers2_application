0fcd598ac2396fcecca9ea66940bb93508b7b834
kunpeisatou <kunpei0411@gmail.com>
2021-10-27 02:31:11 +0000

    userとbooksビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   #get 'users/show'を削除。下のusersとの重複回避のため。
   devise_for :users
   root to: 'homes#top'
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
 
   resources :users, only: [:show, :edit, :update, :index]
 end
