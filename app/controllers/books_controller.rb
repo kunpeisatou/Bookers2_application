edc520f722f712427672c62c2dbed69a04b2d86d
kunpeisatou <kunpei0411@gmail.com>
2021-10-28 08:59:02 +0000

    BookIndex

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,21 +11,22 @@ class BooksController < ApplicationController
       redirect_to book_path(@book.id)
     else
       render :index
-      
+
     end
-    
+
   end
 
   def index
     #@post_images = PostImage.allを削除。左記の場合全ての画像を表示してしまうため。
     @books = Book.page(params[:page]).reverse_order
+
     @user = current_user
-    @book = Book.new
+    @new_book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
-    @user = current_user
+
     @new_book = Book.new
   end
 
@@ -34,12 +35,12 @@ class BooksController < ApplicationController
     @book.destroy
     redirect_to books_path
   end
-  
+
   # 投稿データのストロングパラメータ
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
