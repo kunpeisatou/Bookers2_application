d83dfc006832198fb43ceb9329f8d980aba79630
kunpeisatou <kunpei0411@gmail.com>
2021-10-29 07:30:26 +0000

    ログイン機能とaboutページ

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,4 +2,8 @@ class HomesController < ApplicationController
   def top
     
   end
+  
+  def a
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   #get 'users/show'を削除。下のusersとの重複回避のため。
   devise_for :users
   root to: 'homes#top'
+  get 'home/about' => 'homes#about'
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
 
   resources :users, only: [:show, :edit, :update, :index, :create]
