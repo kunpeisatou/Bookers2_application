0e6f68d8509286b4348fd10b1987fc3a95b55a36
kunpeisatou <kunpei0411@gmail.com>
2021-11-04 10:24:21 +0000

    [finish]いいねコメント修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,16 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to books_path(book)
+    session[:previous_url] = request.referer 
+    redirect_to session[:previous_url]
+   
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to books_path(book)
+    session[:previous_url] = request.referer 
+    redirect_to session[:previous_url]
   end
 end
