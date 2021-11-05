d4d5e51a19d941a0eb42d7557dfd5aeaed024491
kunpeisatou <kunpei0411@gmail.com>
2021-11-05 13:27:00 +0000

    Relationshipモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,13 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  #フォローする側
+  has_many :followers, through: :relationships, source: :followed
+  #フォローしている人一覧
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  #フォローされる側
+  has_many :followeds, through: :reverse_of_relationships, source: :follower
+  #フォローされている人一覧
   attachment :profile_image
 end
