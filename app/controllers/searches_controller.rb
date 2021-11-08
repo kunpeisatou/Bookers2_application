e43eb50284e9a8889e50fae7574873b1c8478dfd
kunpeisatou <kunpei0411@gmail.com>
2021-11-08 04:32:51 +0000

    検索バー

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -28,9 +28,9 @@ class SearchesController < ApplicationController
     # 選択したモデルがbookだったら
     elsif model == 'book'
       if method == 'perfect'
-        Post.where(title: content)
+        Book.where(title: content)
       else
-        Post.where('title LIKE ?', '%'+content+'%')
+        Book.where('title LIKE ?', '%'+content+'%')
       end
     end
   end
